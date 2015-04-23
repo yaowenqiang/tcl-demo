e99abe7e17b863b40c65a6f59c177f9c7e9125fd
yaowenqiang <yaowenqiang111@163.com>
2015-04-24 00:52:57 +0800

    添加了proc函数

--- proc-demo.tcl
+++ proc-demo.tcl
@@ -1,3 +1,4 @@
+#!/usr/bin/env wish
 proc factorial {val} {
     set result 1
     while {$val>0} {
@@ -7,3 +8,14 @@ proc factorial {val} {
     return $result
 }
 
+entry .value -width 6 -relief sunken -textvariable value
+label .description -text "factorial is"
+label .result -textvariable result
+button .calculate -text "Calculate" \
+-command {set result [factorial $value]}
+bind .value <Return> {
+    .calculate flash
+    .calculate invoke
+}
+grid .value .description .result -padx 1m -pady 1m
+grid .calculate - - -padx 1m -pady 1m
